725c2d707e2193246f64e08e2e9891101b873d55
01234567890 <71796281+Auxnos@users.noreply.github.com>
2021-09-15 18:15:31 -0700

    Create GetReal.lua

---
+++ GetReal.lua
@@ -0,0 +1,371 @@
+local mas = script
+Folder0 = Instance.new("Folder")
+Folder1 = Instance.new("Folder")
+ConeHandleAdornment2 = Instance.new("ConeHandleAdornment")
+CylinderHandleAdornment3 = Instance.new("CylinderHandleAdornment")
+CylinderHandleAdornment4 = Instance.new("CylinderHandleAdornment")
+ConeHandleAdornment5 = Instance.new("ConeHandleAdornment")
+Folder6 = Instance.new("Folder")
+ConeHandleAdornment7 = Instance.new("ConeHandleAdornment")
+CylinderHandleAdornment8 = Instance.new("CylinderHandleAdornment")
+CylinderHandleAdornment9 = Instance.new("CylinderHandleAdornment")
+ConeHandleAdornment10 = Instance.new("ConeHandleAdornment")
+Folder11 = Instance.new("Folder")
+ConeHandleAdornment12 = Instance.new("ConeHandleAdornment")
+CylinderHandleAdornment13 = Instance.new("CylinderHandleAdornment")
+CylinderHandleAdornment14 = Instance.new("CylinderHandleAdornment")
+ConeHandleAdornment15 = Instance.new("ConeHandleAdornment")
+Folder16 = Instance.new("Folder")
+ConeHandleAdornment17 = Instance.new("ConeHandleAdornment")
+CylinderHandleAdornment18 = Instance.new("CylinderHandleAdornment")
+CylinderHandleAdornment19 = Instance.new("CylinderHandleAdornment")
+ConeHandleAdornment20 = Instance.new("ConeHandleAdornment")
+Folder21 = Instance.new("Folder")
+ConeHandleAdornment22 = Instance.new("ConeHandleAdornment")
+CylinderHandleAdornment23 = Instance.new("CylinderHandleAdornment")
+CylinderHandleAdornment24 = Instance.new("CylinderHandleAdornment")
+ConeHandleAdornment25 = Instance.new("ConeHandleAdornment")
+Folder26 = Instance.new("Folder")
+ConeHandleAdornment27 = Instance.new("ConeHandleAdornment")
+CylinderHandleAdornment28 = Instance.new("CylinderHandleAdornment")
+CylinderHandleAdornment29 = Instance.new("CylinderHandleAdornment")
+ConeHandleAdornment30 = Instance.new("ConeHandleAdornment")
+Folder0.Name = "ImplementationUI1"
+Folder0.Parent = mas
+Folder1.Name = "PlusY"
+Folder1.Parent = Folder0
+ConeHandleAdornment2.Name = "DimmedHead"
+ConeHandleAdornment2.Parent = Folder1
+ConeHandleAdornment2.CFrame = CFrame.new(0, 3.37964845, 0, 0, -1, 0, 0, 0, -1, 1, 0, 0)
+ConeHandleAdornment2.Color = BrickColor.new("Lime green")
+ConeHandleAdornment2.Transparency = 0.44999998807907
+ConeHandleAdornment2.Radius = 0.17480939626694
+ConeHandleAdornment2.Color3 = Color3.new(0, 0.7, 0)
+ConeHandleAdornment2.ZIndex = 0
+ConeHandleAdornment2.AlwaysOnTop = true
+ConeHandleAdornment2.Height = 0.58269798755646
+CylinderHandleAdornment3.Name = "Shaft"
+CylinderHandleAdornment3.Parent = Folder1
+CylinderHandleAdornment3.CFrame = CFrame.new(0, 2.21425247, 0, 0, -1, 0, 0, 0, -1, 1, 0, 0)
+CylinderHandleAdornment3.Color = BrickColor.new("Lime green")
+CylinderHandleAdornment3.Radius = 0.058269798755646
+CylinderHandleAdornment3.Color3 = Color3.new(0, 0.7, 0)
+CylinderHandleAdornment3.ZIndex = 0
+CylinderHandleAdornment3.Height = 2.3307919502258
+CylinderHandleAdornment4.Name = "DimmedShaft"
+CylinderHandleAdornment4.Parent = Folder1
+CylinderHandleAdornment4.CFrame = CFrame.new(0, 2.21425247, 0, 0, -1, 0, 0, 0, -1, 1, 0, 0)
+CylinderHandleAdornment4.Color = BrickColor.new("Lime green")
+CylinderHandleAdornment4.Transparency = 0.44999998807907
+CylinderHandleAdornment4.Radius = 0.058269798755646
+CylinderHandleAdornment4.Color3 = Color3.new(0, 0.7, 0)
+CylinderHandleAdornment4.ZIndex = 0
+CylinderHandleAdornment4.AlwaysOnTop = true
+CylinderHandleAdornment4.Height = 2.3307919502258
+ConeHandleAdornment5.Name = "Head"
+ConeHandleAdornment5.Parent = Folder1
+ConeHandleAdornment5.CFrame = CFrame.new(0, 3.37964845, 0, 0, -1, 0, 0, 0, -1, 1, 0, 0)
+ConeHandleAdornment5.Color = BrickColor.new("Lime green")
+ConeHandleAdornment5.Radius = 0.17480939626694
+ConeHandleAdornment5.Color3 = Color3.new(0, 0.7, 0)
+ConeHandleAdornment5.ZIndex = 0
+ConeHandleAdornment5.Height = 0.58269798755646
+Folder6.Name = "MinusZ"
+Folder6.Parent = Folder0
+ConeHandleAdornment7.Name = "DimmedHead"
+ConeHandleAdornment7.Parent = Folder6
+ConeHandleAdornment7.CFrame = CFrame.new(0, 0, -3.37964845, 1, 0, 0, 0, 1, 0, 0, 0, 1)
+ConeHandleAdornment7.Color = BrickColor.new("Dark blue")
+ConeHandleAdornment7.Transparency = 0.44999998807907
+ConeHandleAdornment7.Radius = 0.17480939626694
+ConeHandleAdornment7.Color3 = Color3.new(0, 0, 0.7)
+ConeHandleAdornment7.ZIndex = 0
+ConeHandleAdornment7.AlwaysOnTop = true
+ConeHandleAdornment7.Height = 0.58269798755646
+CylinderHandleAdornment8.Name = "Shaft"
+CylinderHandleAdornment8.Parent = Folder6
+CylinderHandleAdornment8.CFrame = CFrame.new(0, 0, -2.21425247, 1, 0, 0, 0, 1, 0, 0, 0, 1)
+CylinderHandleAdornment8.Color = BrickColor.new("Dark blue")
+CylinderHandleAdornment8.Radius = 0.058269798755646
+CylinderHandleAdornment8.Color3 = Color3.new(0, 0, 0.7)
+CylinderHandleAdornment8.ZIndex = 0
+CylinderHandleAdornment8.Height = 2.3307919502258
+CylinderHandleAdornment9.Name = "DimmedShaft"
+CylinderHandleAdornment9.Parent = Folder6
+CylinderHandleAdornment9.CFrame = CFrame.new(0, 0, -2.21425247, 1, 0, 0, 0, 1, 0, 0, 0, 1)
+CylinderHandleAdornment9.Color = BrickColor.new("Dark blue")
+CylinderHandleAdornment9.Transparency = 0.44999998807907
+CylinderHandleAdornment9.Radius = 0.058269798755646
+CylinderHandleAdornment9.Color3 = Color3.new(0, 0, 0.7)
+CylinderHandleAdornment9.ZIndex = 0
+CylinderHandleAdornment9.AlwaysOnTop = true
+CylinderHandleAdornment9.Height = 2.3307919502258
+ConeHandleAdornment10.Name = "Head"
+ConeHandleAdornment10.Parent = Folder6
+ConeHandleAdornment10.CFrame = CFrame.new(0, 0, -3.37964845, 1, 0, 0, 0, 1, 0, 0, 0, 1)
+ConeHandleAdornment10.Color = BrickColor.new("Dark blue")
+ConeHandleAdornment10.Radius = 0.17480939626694
+ConeHandleAdornment10.Color3 = Color3.new(0, 0, 0.7)
+ConeHandleAdornment10.ZIndex = 0
+ConeHandleAdornment10.Height = 0.58269798755646
+Folder11.Name = "MinusY"
+Folder11.Parent = Folder0
+ConeHandleAdornment12.Name = "DimmedHead"
+ConeHandleAdornment12.Parent = Folder11
+ConeHandleAdornment12.CFrame = CFrame.new(0, -3.37964845, 0, 0, 1, 0, 0, 0, 1, 1, 0, 0)
+ConeHandleAdornment12.Color = BrickColor.new("Lime green")
+ConeHandleAdornment12.Transparency = 0.44999998807907
+ConeHandleAdornment12.Radius = 0.17480939626694
+ConeHandleAdornment12.Color3 = Color3.new(0, 0.7, 0)
+ConeHandleAdornment12.ZIndex = 0
+ConeHandleAdornment12.AlwaysOnTop = true
+ConeHandleAdornment12.Height = 0.58269798755646
+CylinderHandleAdornment13.Name = "Shaft"
+CylinderHandleAdornment13.Parent = Folder11
+CylinderHandleAdornment13.CFrame = CFrame.new(0, -2.21425247, 0, 0, 1, 0, 0, 0, 1, 1, 0, 0)
+CylinderHandleAdornment13.Color = BrickColor.new("Lime green")
+CylinderHandleAdornment13.Radius = 0.058269798755646
+CylinderHandleAdornment13.Color3 = Color3.new(0, 0.7, 0)
+CylinderHandleAdornment13.ZIndex = 0
+CylinderHandleAdornment13.Height = 2.3307919502258
+CylinderHandleAdornment14.Name = "DimmedShaft"
+CylinderHandleAdornment14.Parent = Folder11
+CylinderHandleAdornment14.CFrame = CFrame.new(0, -2.21425247, 0, 0, 1, 0, 0, 0, 1, 1, 0, 0)
+CylinderHandleAdornment14.Color = BrickColor.new("Lime green")
+CylinderHandleAdornment14.Transparency = 0.44999998807907
+CylinderHandleAdornment14.Radius = 0.058269798755646
+CylinderHandleAdornment14.Color3 = Color3.new(0, 0.7, 0)
+CylinderHandleAdornment14.ZIndex = 0
+CylinderHandleAdornment14.AlwaysOnTop = true
+CylinderHandleAdornment14.Height = 2.3307919502258
+ConeHandleAdornment15.Name = "Head"
+ConeHandleAdornment15.Parent = Folder11
+ConeHandleAdornment15.CFrame = CFrame.new(0, -3.37964845, 0, 0, 1, 0, 0, 0, 1, 1, 0, 0)
+ConeHandleAdornment15.Color = BrickColor.new("Lime green")
+ConeHandleAdornment15.Radius = 0.17480939626694
+ConeHandleAdornment15.Color3 = Color3.new(0, 0.7, 0)
+ConeHandleAdornment15.ZIndex = 0
+ConeHandleAdornment15.Height = 0.58269798755646
+Folder16.Name = "PlusZ"
+Folder16.Parent = Folder0
+ConeHandleAdornment17.Name = "DimmedHead"
+ConeHandleAdornment17.Parent = Folder16
+ConeHandleAdornment17.CFrame = CFrame.new(0, 0, 3.37964845, 1, 0, 0, 0, -1, 0, 0, 0, -1)
+ConeHandleAdornment17.Color = BrickColor.new("Dark blue")
+ConeHandleAdornment17.Transparency = 0.44999998807907
+ConeHandleAdornment17.Radius = 0.17480939626694
+ConeHandleAdornment17.Color3 = Color3.new(0, 0, 0.7)
+ConeHandleAdornment17.ZIndex = 0
+ConeHandleAdornment17.AlwaysOnTop = true
+ConeHandleAdornment17.Height = 0.58269798755646
+CylinderHandleAdornment18.Name = "Shaft"
+CylinderHandleAdornment18.Parent = Folder16
+CylinderHandleAdornment18.CFrame = CFrame.new(0, 0, 2.21425247, 1, 0, 0, 0, -1, 0, 0, 0, -1)
+CylinderHandleAdornment18.Color = BrickColor.new("Dark blue")
+CylinderHandleAdornment18.Radius = 0.058269798755646
+CylinderHandleAdornment18.Color3 = Color3.new(0, 0, 0.7)
+CylinderHandleAdornment18.ZIndex = 0
+CylinderHandleAdornment18.Height = 2.3307919502258
+CylinderHandleAdornment19.Name = "DimmedShaft"
+CylinderHandleAdornment19.Parent = Folder16
+CylinderHandleAdornment19.CFrame = CFrame.new(0, 0, 2.21425247, 1, 0, 0, 0, -1, 0, 0, 0, -1)
+CylinderHandleAdornment19.Color = BrickColor.new("Dark blue")
+CylinderHandleAdornment19.Transparency = 0.44999998807907
+CylinderHandleAdornment19.Radius = 0.058269798755646
+CylinderHandleAdornment19.Color3 = Color3.new(0, 0, 0.7)
+CylinderHandleAdornment19.ZIndex = 0
+CylinderHandleAdornment19.AlwaysOnTop = true
+CylinderHandleAdornment19.Height = 2.3307919502258
+ConeHandleAdornment20.Name = "Head"
+ConeHandleAdornment20.Parent = Folder16
+ConeHandleAdornment20.CFrame = CFrame.new(0, 0, 3.37964845, 1, 0, 0, 0, -1, 0, 0, 0, -1)
+ConeHandleAdornment20.Color = BrickColor.new("Dark blue")
+ConeHandleAdornment20.Radius = 0.17480939626694
+ConeHandleAdornment20.Color3 = Color3.new(0, 0, 0.7)
+ConeHandleAdornment20.ZIndex = 0
+ConeHandleAdornment20.Height = 0.58269798755646
+Folder21.Name = "MinusX"
+Folder21.Parent = Folder0
+ConeHandleAdornment22.Name = "DimmedHead"
+ConeHandleAdornment22.Parent = Folder21
+ConeHandleAdornment22.CFrame = CFrame.new(-3.37964845, 0, 0, 0, 0, 1, 1, 0, 0, 0, 1, 0)
+ConeHandleAdornment22.Color = BrickColor.new("Crimson")
+ConeHandleAdornment22.Transparency = 0.44999998807907
+ConeHandleAdornment22.Radius = 0.17480939626694
+ConeHandleAdornment22.Color3 = Color3.new(0.7, 0, 0)
+ConeHandleAdornment22.ZIndex = 0
+ConeHandleAdornment22.AlwaysOnTop = true
+ConeHandleAdornment22.Height = 0.58269798755646
+CylinderHandleAdornment23.Name = "Shaft"
+CylinderHandleAdornment23.Parent = Folder21
+CylinderHandleAdornment23.CFrame = CFrame.new(-2.21425247, 0, 0, 0, 0, 1, 1, 0, 0, 0, 1, 0)
+CylinderHandleAdornment23.Color = BrickColor.new("Crimson")
+CylinderHandleAdornment23.Radius = 0.058269798755646
+CylinderHandleAdornment23.Color3 = Color3.new(0.7, 0, 0)
+CylinderHandleAdornment23.ZIndex = 0
+CylinderHandleAdornment23.Height = 2.3307919502258
+CylinderHandleAdornment24.Name = "DimmedShaft"
+CylinderHandleAdornment24.Parent = Folder21
+CylinderHandleAdornment24.CFrame = CFrame.new(-2.21425247, 0, 0, 0, 0, 1, 1, 0, 0, 0, 1, 0)
+CylinderHandleAdornment24.Color = BrickColor.new("Crimson")
+CylinderHandleAdornment24.Transparency = 0.44999998807907
+CylinderHandleAdornment24.Radius = 0.058269798755646
+CylinderHandleAdornment24.Color3 = Color3.new(0.7, 0, 0)
+CylinderHandleAdornment24.ZIndex = 0
+CylinderHandleAdornment24.AlwaysOnTop = true
+CylinderHandleAdornment24.Height = 2.3307919502258
+ConeHandleAdornment25.Name = "Head"
+ConeHandleAdornment25.Parent = Folder21
+ConeHandleAdornment25.CFrame = CFrame.new(-3.37964845, 0, 0, 0, 0, 1, 1, 0, 0, 0, 1, 0)
+ConeHandleAdornment25.Color = BrickColor.new("Crimson")
+ConeHandleAdornment25.Radius = 0.17480939626694
+ConeHandleAdornment25.Color3 = Color3.new(0.7, 0, 0)
+ConeHandleAdornment25.ZIndex = 0
+ConeHandleAdornment25.Height = 0.58269798755646
+Folder26.Name = "PlusX"
+Folder26.Parent = Folder0
+ConeHandleAdornment27.Name = "DimmedHead"
+ConeHandleAdornment27.Parent = Folder26
+ConeHandleAdornment27.CFrame = CFrame.new(3.37964845, 0, 0, 0, 0, -1, 1, 0, 0, 0, -1, 0)
+ConeHandleAdornment27.Color = BrickColor.new("Crimson")
+ConeHandleAdornment27.Transparency = 0.44999998807907
+ConeHandleAdornment27.Radius = 0.17480939626694
+ConeHandleAdornment27.Color3 = Color3.new(0.7, 0, 0)
+ConeHandleAdornment27.ZIndex = 0
+ConeHandleAdornment27.AlwaysOnTop = true
+ConeHandleAdornment27.Height = 0.58269798755646
+CylinderHandleAdornment28.Name = "Shaft"
+CylinderHandleAdornment28.Parent = Folder26
+CylinderHandleAdornment28.CFrame = CFrame.new(2.21425247, 0, 0, 0, 0, -1, 1, 0, 0, 0, -1, 0)
+CylinderHandleAdornment28.Color = BrickColor.new("Crimson")
+CylinderHandleAdornment28.Radius = 0.058269798755646
+CylinderHandleAdornment28.Color3 = Color3.new(0.7, 0, 0)
+CylinderHandleAdornment28.ZIndex = 0
+CylinderHandleAdornment28.Height = 2.3307919502258
+CylinderHandleAdornment29.Name = "DimmedShaft"
+CylinderHandleAdornment29.Parent = Folder26
+CylinderHandleAdornment29.CFrame = CFrame.new(2.21425247, 0, 0, 0, 0, -1, 1, 0, 0, 0, -1, 0)
+CylinderHandleAdornment29.Color = BrickColor.new("Crimson")
+CylinderHandleAdornment29.Transparency = 0.44999998807907
+CylinderHandleAdornment29.Radius = 0.058269798755646
+CylinderHandleAdornment29.Color3 = Color3.new(0.7, 0, 0)
+CylinderHandleAdornment29.ZIndex = 0
+CylinderHandleAdornment29.AlwaysOnTop = true
+CylinderHandleAdornment29.Height = 2.3307919502258
+ConeHandleAdornment30.Name = "Head"
+ConeHandleAdornment30.Parent = Folder26
+ConeHandleAdornment30.CFrame = CFrame.new(3.37964845, 0, 0, 0, 0, -1, 1, 0, 0, 0, -1, 0)
+ConeHandleAdornment30.Color = BrickColor.new("Crimson")
+ConeHandleAdornment30.Radius = 0.17480939626694
+ConeHandleAdornment30.Color3 = Color3.new(0.7, 0, 0)
+ConeHandleAdornment30.ZIndex = 0
+ConeHandleAdornment30.Height = 0.58269798755646
+for i,v in pairs(Folder0:GetDescendants()) do
+    pcall(function()
+        local epic = v.CFrame
+        task.spawn(function()
+            local Adornee = nil
+            for _, p in pairs(owner.Character:GetDescendants()) do
+                if p:IsA("BasePart") then
+                    v.CFrame = epic
+                    v.Adornee = p
+                    Adornee = p
+                end
+            end
+            owner.CharacterAdded:connect(function()
+                for _, p in pairs(owner.Character:GetDescendants()) do
+                    if p:IsA("BasePart") then
+                        v.CFrame = epic
+                        v.Adornee = p
+                        Adornee = p
+                    end
+                end
+            end)
+            while v:IsDescendantOf(script) do
+                if not Adornee then
+                    for _, p in pairs(owner.Character:GetDescendants()) do
+                        if p:IsA("BasePart") then
+                            v.CFrame = epic
+                            v.Adornee = p
+                            Adornee = p
+                        end
+                    end
+                end
+                v.CFrame = epic
+                v.Adornee = Adornee
+                task.wait()
+            end
+        end)
+    end)
+end
+local Where = CFrame.new(0, 1,0 )
+local Mainpos = CFrame.new(0,0,0)
+pcall(function()
+    owner.FloppaRemote:Destroy()
+end)
+Player= owner
+do
+    local FloppaRemote = Instance.new("RemoteEvent")
+    FloppaRemote.Name = "FloppaRemote"
+    FloppaRemote.Parent = Player
+    FloppaRemote.OnServerEvent:connect(function(plr, ...)
+        local args = { ... }
+        if args[1] == "Move" then
+            Mainpos = args[2]
+            end
+    end)
+    local ClientCode = [==[
+    task.wait()
+    script.Parent = nil
+    task.wait(1.5)
+    task.spawn(function()
+        local function KeyDown(Key)
+            return not game:GetService("UserInputService"):GetFocusedTextBox() and game:GetService("UserInputService"):IsKeyDown(Enum.KeyCode[Key]) or false
+        end
+        local What = false
+        Mainpos = CFrame.new(0, 0, 0)
+        local Mouse = owner:GetMouse()
+        while task.wait() do
+            LastPos = Mainpos
+            if What == true then
+                Event2:FireServer("KillAllInArea",Mainpos)
+            end
+            if KeyDown("W") then
+                Mainpos = Mainpos * CFrame.new(0,0,-1)
+            end
+            if KeyDown("S") then
+                Mainpos = Mainpos *CFrame.new(0,0,1)
+            end
+            if KeyDown("A") then
+                Mainpos = Mainpos *CFrame.new(-1,0,0)
+            end
+            if KeyDown("D") then
+                Mainpos = Mainpos *CFrame.new(1,0,0)
+            end
+            if Mainpos ~= LastPos or game:GetService("UserInputService").MouseBehavior.Name ~= "Default" then
+                Mainpos = CFrame.new(Mainpos.p,Mainpos.p+workspace.CurrentCamera.CFrame.LookVector)
+            end
+            owner.FloppaRemote:FireServer("Move",Mainpos)
+workspace.CurrentCamera.CameraSubject = workspace["TinyFlop".. owner.Name]:FindFirstChild("Floppa cube")
+        end
+    end)
+    ]==]
+    if #ClientCode > 1 then
+        NLS(ClientCode,Player.PlayerGui)
+    end
+end
+
+while task.wait() do
+    for i,v in pairs(owner.Character:GetDescendants()) do
+        pcall(function()
+            v.Anchored = true
+            v.CFrame = Mainpos
+        end)
+        pcall(function()
+            v.Enabled = false
+        end)
+        pcall(function()
+            v.Transparency = math.random(1,100)
+        end)
+    end
+end
